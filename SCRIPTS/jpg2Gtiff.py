faf5eb56f3f7c7c8f19ca3ec9fa6ea2539670230
Monia Molinari <moni.molinari@gmail.com>
2021-05-10 16:09:37 +0200

    Update jpg2Gtiff.py

--- SCRIPTS/jpg2Gtiff.py
+++ SCRIPTS/jpg2Gtiff.py
@@ -86,8 +86,6 @@ print("...Masking performed.\n")
 
 # Save rescaled GeoTiff
 profile = src.profile.copy()
-if profile['nodata']<0 or profile['nodata']>255:
-    del profile['nodata']
 profile.update(
    dtype='float64',
    nodata=-9999,
